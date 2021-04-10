e560ff52f538091cef910dbfdc0c4eb1980787c3
Sukharevich Vladislav <vladoslav.sukharevich3@gmail.com>
2021-04-10 21:08:34 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task12_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Func<bool> func = null;
+            func += F;
+
+            Action<Func<bool>, string, int> myDel = null;
+            myDel += ((a, b, c) =>
+                Console.WriteLine($"{a}\t{b}\t{c}")
+            );
+
+            myDel += ((a, b, c) =>
+                Console.WriteLine($"{a}\t{b}\t{c}")
+            );
+
+            myDel(func, "ABOBA", 5);
+        }
+        static bool F()
+        {
+            return false;
+        }
+    }
+}
